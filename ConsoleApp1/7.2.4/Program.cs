c63399cafb26af1d7281eb70e0bd5407d6e7f84f
Nestjik <Tesl@physicist.net>
2019-03-06 14:27:44 +0100

    zadaca 7.1

---
+++ ConsoleApp1/7.2.4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2._4
+{
+    class Program
+    {
+        static double CtoF(double c)
+        {
+            return c * 1.8 + 32;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Unesi Celzijeve: ");
+            double c = double.Parse(Console.ReadLine());
+
+            Console.Write("Fahrenheitovi: " + CtoF(c));
+            Console.Read();
+        }
+    }
+}
---
+++ ConsoleApp1/7.2.6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2._6
+{
+    class Program
+    {
+        static bool DalSiProst(int num)
+        {
+            if (num < 2)
+            {
+                return false;
+            }
+            int dj = 2;
+            while (num > dj)
+            {
+                if (num % dj == 0)
+                {
+                    return false;
+                }
+                dj++;
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DalSiProst(5));
+            Console.Read();
+        }
+    }
+}
