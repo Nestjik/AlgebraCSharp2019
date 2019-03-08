cf4da7abcc9d9604ccfc6557f0c8d0c2b0fbed76
Nestjit <Tesl@physicist.net>
2019-03-08 18:23:05 +0100

    test

---
+++ ConsoleApp1/test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nesto = new List<string>();
+            nesto.Add("fds");
+            nesto.Add("fgf");
+            nesto.ForEach(delegate(string n){
+                Console.WriteLine(n);
+            });
+            Console.Read();
+        }
+    }
+}
