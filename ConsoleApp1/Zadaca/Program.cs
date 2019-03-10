94f1096bc54b660dfddcf2ef54517bb24905d007
Nestjik <Tesl@physicist.net>
2019-03-10 20:33:06 +0100

    zadaca ucenik

---
+++ ConsoleApp1/Zadaca/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadaca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ucenik u1 = new Ucenik("Marko", "Markic");
+            Ucenik u2 = new Ucenik("Ivan", "Ivancic", 12345678910);
+            Ucenik u3 = new Ucenik("Fdgra", "Dhrtse", 10987654321, 8000);
+
+            List<Ucenik> listaUcenik = new List<Ucenik> { u1, u2, u3 };
+
+            int i = 1;
+            listaUcenik.ForEach(u => Ispis(u, i++));
+
+            listaUcenik.ForEach(delegate (Ucenik u)
+            {
+                u.Racun -= 1000;
+            });
+
+            i = 1;
+            Console.WriteLine();
+            listaUcenik.ForEach(u => Ispis(u, i++));
+
+            Console.ReadKey();
+        }
+
+        private static void Ispis(Ucenik u, int i)
+        {
+            Console.WriteLine($"{i}. {u}");
+        }
+    }
+}
---
+++ ConsoleApp1/Zadaca/Ucenik.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadaca
+{
+    internal class Ucenik
+    {
+        public Ucenik(string i, string p, long o=0, int? r=null)
+        {
+            this.ime = i;
+            this.prezime = p;
+            this.Oib = o;
+
+            if (r == null)
+            {
+                this.Racun = rand.Next(2000, 6001);
+            }
+            else
+            {
+                this.Racun = (int)r;
+            }
+        }
+
+        private string ime;
+        public string Ime { get => ime; }
+
+        private string prezime;
+        public string Prezime { get => prezime; }
+
+        private long oib;
+        public long Oib { set => oib = value; }
+
+        private int racun;
+        public int Racun { get => racun; set => racun = value; }
+
+        public string telefon;
+
+        private static Random rand = new Random();
+
+        public override string ToString()
+        {
+            return $"Pozdrav, {this.Ime} {this.Prezime}! Stanje vaseg racuna iznosi {this.Racun} kn.";
+        }
+    }
+}
