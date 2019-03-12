885c2b8bd554ca781d5ffab48105aa908e243ae5
Nestjit <Tesl@physicist.net>
2019-03-12 17:09:55 +0100

    8.3.1

---
+++ ConsoleApp1/8.2.2.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._2._2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Inicijaliziraj(out int a);
+            Console.WriteLine("a = {0}", a);
+            Console.ReadKey();
+        }
+        private static void Inicijaliziraj(out int a)
+        {
+            a = 1;
+        }
+    }
+}
